99bbf9990c4ee370cd25cf2e7a10d0d3d0bd4741
pfeinman <peter.feinman@gmail.com>
2019-06-13 23:20:37 -0700

    cherry pick pete msgs pt 1

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,17 @@ Spree::Core::Engine.add_routes do
     get "/messages/support" => "messages#message_support"
     get "pages/about_us" => "pages#about_us"
 
+    resources :messages, only: [:index] do
+      resources :message_support, only: [:index]
+    end 
+
+    get "/messages" => "messages#index"
+    get "/messages/support" => "messages#message_support"
+
+    # kludge
+    get "/messages/one" => "messages#thread_list_one"
+    get "/messages/two" => "messages#thread_list_two"
+
     resources :live_stream do
       collection do
         get :generate_playback
@@ -55,4 +66,13 @@ Spree::Core::Engine.add_routes do
     end
   end
 
+    # This line mounts Spree's routes at the root of your application.
+    # This means, any requests to URLs such as /products, will go to
+    # Spree::ProductsController.
+    # If you would like to change where this engine is mounted, simply change the
+    # :at option to something different.
+    #
+    # We ask that you don't use the :as option here, as Spree relies on it being
+    # the default of "spree".
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
