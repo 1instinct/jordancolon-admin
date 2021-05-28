6c4fe12784c7edb682f71c3283baeecf0c4de5a8
David Dombrowsky <davek@6thstreetradio.org>
2021-05-27 21:26:42 -0400

    added notes about k8 startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,17 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+echo Micro Kubernetes Container Startup
 echo RAILS_ENV=$RAILS_ENV
 
 if [ ! -e assets_precompiled ] ; then
     rails assets:precompile
     touch assets_precompiled
+    # for the k8 deploy, we run this initially to build the assets and
+    # then store the container so it can be scaled.  For normal
+    # operation, this should not exist here but continue on to
+    # start the web server.  TODO: there might be a better way to
+    # do this.
     exit 0
 fi
 
