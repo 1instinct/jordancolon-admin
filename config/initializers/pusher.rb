e900840a01b62c34444a4418889801e0b5ad51e4
mayankg-protonshub <mayank.g@protonshub.com>
2021-08-10 19:50:30 +0530

    added configiuration code for the pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,12 +1,10 @@
-# require 'pusher'
+require 'pusher'
 
-# raise "PusherKeysMissingException" if PUSHER_APP_ID.empty? || PUSHER_APP_ID != ENV['PUSHER_APP_ID']
+raise "PusherKeysMissingException" if PUSHER_APP_ID.empty? || PUSHER_APP_ID != ENV['PUSHER_APP_ID']
 
-# Pusher.app_id = ENV['PUSHER_APP_ID']
-# Pusher.key = ENV['PUSHER_KEY']
-# Pusher.secret = ENV['PUSHER_SECRET']
-# Pusher.cluster = ENV['PUSHER_CLUSTER']
-# Pusher.logger = Rails.logger
-# Pusher.encrypted = true
-
-
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
+Pusher.cluster = ENV['PUSHER_CLUSTER']
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
